be7a8174601d3fc735d72d32f36441c21fabb126
eson <474420502@qq.com>
2023-06-18 23:51:38 +0800

    添加多线程控制

--- multi_pool.go
+++ multi_pool.go
@@ -1,17 +1,14 @@
 package requests
 
 import (
-	"context"
 	"log"
 	"sync"
-	"time"
 
 	"github.com/schollz/progressbar"
 )
 
 type RequestPool struct {
 	runnerCount int
-	timeout     time.Duration
 
 	temps []*Temporary
 	lock  sync.Mutex
@@ -32,26 +29,18 @@ func (pl *RequestPool) Add(tp *Temporary) {
 	pl.lock.Unlock()
 }
 
-func (pl *RequestPool) SetPerTimeout(dur time.Duration) {
-	pl.timeout = dur
-}
-
 func (pl *RequestPool) Execute(errHandler func(int, error)) []*Response {
 	pl.lock.Lock()
 	defer pl.lock.Unlock()
-	ctx, cancel := context.WithTimeout(context.Background(), pl.timeout)
-	defer cancel()
 
 	respChan := make(chan *Response, len(pl.temps))
 	var result []*Response
-
-	timeout := time.After(pl.timeout)
 
 	bar := progressbar.New(len(pl.temps))
 
 	for i, tp := range pl.temps {
 		pl.sem <- 1
-		ctx, cancel := context.WithTimeout(ctx, pl.timeout)
+
 		go func(i int, tp *Temporary) {
 			defer func() {
 				<-pl.sem
@@ -72,12 +61,7 @@ func (pl *RequestPool) Execute(errHandler func(int, error)) []*Response {
 
 	// 从channel中接收响应,超时则退出循环
 	for range pl.temps {
-		select {
-		case r := <-respChan:
-			result = append(result, r)
-		case <-timeout:
-			return result
-		}
+		result = append(result, <-respChan)
 	}
 
 	return result
--- multi_pool_test.go
+++ multi_pool_test.go
@@ -10,6 +10,7 @@ func TestRequestPool(t *testing.T) {
 	pool := NewRequestPool(3) // 设置最大并发数3
 
 	ses := NewSession()
+	ses.Config().SetTimeout(3 * time.Second)
 	// 添加6个GET请求到httpbin
 	urls := []string{"https://httpbin.org/get",
 		"https://httpbin.org/get",
@@ -18,9 +19,6 @@ func TestRequestPool(t *testing.T) {
 		tp := ses.Get(url)
 		pool.Add(tp)
 	}
-
-	// 设置1秒超时
-	pool.SetPerTimeout(5 * time.Second)
 
 	// 执行并验证正确的响应
 	resps := pool.Execute(nil)
@@ -35,8 +33,8 @@ func TestRequestPool(t *testing.T) {
 	}
 
 	// 添加两个故意超时的请求
-	pool.Add(ses.Get("https://httpbin.org/delay/6"))
-	pool.Add(ses.Get("https://httpbin.org/delay/7"))
+	pool.Add(ses.Get("https://httpbin.org/delay/4"))
+	pool.Add(ses.Get("https://httpbin.org/delay/5"))
 
 	resps = pool.Execute(nil)
 	if resps[1] != nil || resps[2] != nil {
