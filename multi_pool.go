1f7e628b33b609311a96ce932e7519f1d6dc671a
eson <474420502@qq.com>
2023-06-19 00:36:26 +0800

    添加进度条控制开关

--- multi_pool.go
+++ multi_pool.go
@@ -9,15 +9,16 @@ import (
 
 type RequestPool struct {
 	runnerCount int
+	isBar       bool
 
 	temps []*Temporary
 	lock  sync.Mutex
-
-	sem chan int
+	sem   chan int
 }
 
 func NewRequestPool(runnerCount int) *RequestPool {
 	return &RequestPool{
+		isBar:       true,
 		runnerCount: runnerCount,
 		sem:         make(chan int, runnerCount),
 	}
@@ -29,6 +30,12 @@ func (pl *RequestPool) Add(tp *Temporary) {
 	pl.lock.Unlock()
 }
 
+func (pl *RequestPool) SetBar(is bool) {
+	pl.lock.Lock()
+	pl.isBar = is
+	pl.lock.Unlock()
+}
+
 func (pl *RequestPool) Execute(errHandler func(int, error)) []*Response {
 	pl.lock.Lock()
 	defer pl.lock.Unlock()
@@ -36,7 +43,10 @@ func (pl *RequestPool) Execute(errHandler func(int, error)) []*Response {
 	respChan := make(chan *Response, len(pl.temps))
 	var result []*Response
 
-	bar := progressbar.New(len(pl.temps))
+	var bar *progressbar.ProgressBar
+	if pl.isBar {
+		bar = progressbar.New(len(pl.temps))
+	}
 
 	for i, tp := range pl.temps {
 		pl.sem <- 1
@@ -44,7 +54,9 @@ func (pl *RequestPool) Execute(errHandler func(int, error)) []*Response {
 		go func(i int, tp *Temporary) {
 			defer func() {
 				<-pl.sem
-				bar.Add(1) // 完成一个请求则进度+1
+				if pl.isBar {
+					bar.Add(1) // 完成一个请求则进度+1
+				}
 			}()
 			r, err := tp.Execute()
 			if err != nil {
